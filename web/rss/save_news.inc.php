29101f580ee9a2674dcf4674da58924813794723
anton <sowander1986@gmail.com>
2015-12-17 19:19:20 +0600

    исправил косяк обработки

--- web/rss/save_news.inc.php
+++ web/rss/save_news.inc.php
@@ -3,16 +3,19 @@ $t = $news->clearStr($_POST['title']);
 $d = $news->clearStr($_POST['description']);
 $s = $news->clearStr($_POST['source']);
 $c = $news->clearInt($_POST['category']);
-if(empty($t) or empty($d)){
+if(empty($t) or empty($d)) {
     $errMsg = 'Заполни поля!';
 }else{
-if(!$news->saveNews($t, $c, $d, $s)){
-    $errMsg = 'ну ёптить';
-}else{
-    //echo 'ok';
-    //echo $t, $d, $s, $c;
-    header('Location: news.php');
-    exit;
-}}
+//$news->saveNews($t, $c, $d, $s);
+    if($news->saveNews($t, $c, $d, $s==false)){
+        $errMsg = 'ну ёптить';
+    }else {
+        //$errMsg = 'ну ёптить';
+        //echo 'ok';
+        //echo $t, $d, $s, $c;
+        header('Location: news.php');
+        exit;
+    }
+}
 
 ?>
